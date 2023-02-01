a3517670b507fb1c55607804464fcbd4bf2e730d
JakeTurnick <jaketurnick@gmail.com>
2023-02-01 09:02:27 -0500

    cleared warnings

--- src/components/img-board.js
+++ src/components/img-board.js
@@ -1,15 +1,14 @@
-import "./img-board.css"
+import "./img-board.css";
 
 function ImgBoard(props) {
-
-    return (
-        <li>
-            <div className="card-body">
-                <img className="img" src={props.imgSrc} />
-                <p className="img-caption">{props.imgName}</p>
-            </div>
-        </li>
-    )
+  return (
+    <li>
+      <div className="card-body">
+        <img className="img" src={props.imgSrc} alt="" />
+        <p className="img-caption">{props.imgName}</p>
+      </div>
+    </li>
+  );
 }
 
 export default ImgBoard;
